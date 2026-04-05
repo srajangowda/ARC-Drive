5e81ac729bd21246588b7f28d4568411cc2b4bee
srajangowda <srajangowda95@gmail.com>
2026-04-05 19:18:26 +0530

    feat: configure EC2 CI/CD pipeline

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/ARC-Drive/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
